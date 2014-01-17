bcf6d4c0c4aca8dac1377c969b198acbae954bdd
Michael Banzon <michael@banzon.dk>
2014-01-17 05:40:46 +0100

    Parsing moved to new file.

--- currency.go
+++ currency.go
@@ -5,34 +5,8 @@
 package currency
 
 import (
-	"encoding/xml"
-	"fmt"
-	"github.com/mbanzon/simplehttp"
 	"time"
 )
-
-const (
-	ecbResourceUrl = "http://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml"
-	dateFormat     = "2006-01-02"
-)
-
-type currencyEnvelope struct {
-	Sender string `xml:"Sender>name"`
-	Cube   []cube `xml:"Cube>Cube>Cube"`
-}
-
-type timeEnvelope struct {
-	Time timeCube `xml:"Cube>Cube"`
-}
-
-type timeCube struct {
-	Time string `xml:"time,attr"`
-}
-
-type cube struct {
-	Name string  `xml:"currency,attr"`
-	Rate float64 `xml:"rate,attr"`
-}
 
 type CurrencyConverter struct {
 	date       time.Time
@@ -40,35 +14,10 @@ type CurrencyConverter struct {
 }
 
 func NewConverter() (*CurrencyConverter, error) {
-	r := simplehttp.NewGetRequest(ecbResourceUrl)
-	data, err := r.MakeRequest()
+	currencyTime, currencies, err := parseEcbData()
 	if err != nil {
 		return nil, err
 	}
-
-	var c currencyEnvelope
-	err = xml.Unmarshal(data, &c)
-	if err != nil {
-		return nil, err
-	}
-
-	var t timeEnvelope
-	err = xml.Unmarshal(data, &t)
-	if err != nil {
-		return nil, err
-	}
-
-	currencyTime, err := time.Parse(dateFormat, t.Time.Time)
-	if err != nil {
-		return nil, err
-	}
-
-	currencies := make(map[string]float64)
-
-	for _, currency := range c.Cube {
-		currencies[currency.Name] = currency.Rate
-	}
-
 	converter := CurrencyConverter{date: currencyTime, currencies: currencies}
 	return &converter, nil
 }
---
+++ parsing.go
@@ -0,0 +1,63 @@
+package currency
+
+import (
+	"encoding/xml"
+	"github.com/mbanzon/simplehttp"
+	"time"
+)
+
+const (
+	ecbResourceUrl = "http://www.ecb.europa.eu/stats/eurofxref/eurofxref-daily.xml"
+	dateFormat     = "2006-01-02"
+)
+
+type currencyEnvelope struct {
+	Sender string `xml:"Sender>name"`
+	Cube   []cube `xml:"Cube>Cube>Cube"`
+}
+
+type timeEnvelope struct {
+	Time timeCube `xml:"Cube>Cube"`
+}
+
+type timeCube struct {
+	Time string `xml:"time,attr"`
+}
+
+type cube struct {
+	Name string  `xml:"currency,attr"`
+	Rate float64 `xml:"rate,attr"`
+}
+
+func parseEcbData() (time.Time, map[string]float64, error) {
+	r := simplehttp.NewGetRequest(ecbResourceUrl)
+	data, err := r.MakeRequest()
+	if err != nil {
+		return time.Time{}, nil, err
+	}
+
+	var c currencyEnvelope
+	err = xml.Unmarshal(data, &c)
+	if err != nil {
+		return time.Time{}, nil, err
+	}
+
+	var t timeEnvelope
+	err = xml.Unmarshal(data, &t)
+	if err != nil {
+		return time.Time{}, nil, err
+	}
+
+	currencyTime, err := time.Parse(dateFormat, t.Time.Time)
+	if err != nil {
+		return time.Time{}, nil, err
+	}
+
+	currencies := make(map[string]float64)
+
+	for _, currency := range c.Cube {
+		currencies[currency.Name] = currency.Rate
+	}
+
+	return currencyTime, currencies, nil
+}
