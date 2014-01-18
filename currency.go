00a7864696da9657e6c0b5cff8faf1bc81dbf795
Michael Banzon <michael@banzon.dk>
2014-01-18 15:06:54 +0100

    Added convert method. Added converter type for conversion between to fixed currencies.

--- currency.go
+++ currency.go
@@ -5,6 +5,8 @@
 package currency
 
 import (
+	"errors"
+	"fmt"
 	"time"
 )
 
@@ -18,6 +20,12 @@ type CurrencyConverter struct {
 	currencies map[string]float64
 }
 
+type SingleCurrencyConverter struct {
+	date             time.Time
+	from, to         string
+	fromRate, toRate float64
+}
+
 func NewConverter() (*CurrencyConverter, error) {
 	currencyTime, currencies, err := parseEcbData()
 	if err != nil {
@@ -27,6 +35,34 @@ func NewConverter() (*CurrencyConverter, error) {
 	return &converter, nil
 }
 
-func (c *CurrencyConverter) Convert(amount float64, to string, from string) (float64, error) {
-	return 0, nil
+func (c *CurrencyConverter) Convert(amount float64, from string, to string) (float64, error) {
+	fromRate, fromOk := c.currencies[from]
+	if !fromOk {
+		return 0, errors.New(fmt.Sprintf(unknown, from))
+	}
+
+	toRate, toOk := c.currencies[to]
+	if !toOk {
+		return 0, errors.New(fmt.Sprintf(unknown, to))
+	}
+
+	return amount / fromRate * toRate, nil
+}
+
+func (c *CurrencyConverter) GetSingleCurrencyConverter(from, to string) (*SingleCurrencyConverter, error) {
+	fromRate, fromOk := c.currencies[from]
+	if !fromOk {
+		return nil, errors.New(fmt.Sprintf(unknown, from))
+	}
+	toRate, toOk := c.currencies[to]
+	if !toOk {
+		return nil, errors.New(fmt.Sprintf(unknown, to))
+	}
+
+	converter := SingleCurrencyConverter{date: c.date, from: from, to: to, fromRate: fromRate, toRate: toRate}
+	return &converter, nil
+}
+
+func (c *SingleCurrencyConverter) Convert(amount float64) float64 {
+	return amount / c.fromRate * c.toRate
 }
