9a63743ea73ee1b35a9f391abfa45e3d98f9c6e2
Michael Banzon <michael@banzon.dk>
2014-05-10 23:18:48 +0200

    Beefed up the test a bit.

---
+++ dummydata_test.go
@@ -0,0 +1,35 @@
+package currency
+
+var missingCurrenciesXml string = `<?xml version="1.0" encoding="UTF-8"?>
+<gesmes:Envelope xmlns:gesmes="http://www.gesmes.org/xml/2002-08-01" xmlns="http://www.ecb.int/vocabulary/2002-08-01/eurofxref">
+	<gesmes:subject>Reference rates</gesmes:subject>
+	<gesmes:Sender>
+		<gesmes:name>European Central Bank</gesmes:name>
+	</gesmes:Sender>
+	<Cube>
+		<Cube time='2014-05-09'>
+		</Cube>
+	</Cube>
+</gesmes:Envelope>`
+
+var missingCurrencyBlockXml string = `<?xml version="1.0" encoding="UTF-8"?>
+<gesmes:Envelope xmlns:gesmes="http://www.gesmes.org/xml/2002-08-01" xmlns="http://www.ecb.int/vocabulary/2002-08-01/eurofxref">
+	<gesmes:subject>Reference rates</gesmes:subject>
+	<gesmes:Sender>
+		<gesmes:name>European Central Bank</gesmes:name>
+	</gesmes:Sender>
+</gesmes:Envelope>`
+
+var invalidTimeXml string = `<?xml version="1.0" encoding="UTF-8"?>
+<gesmes:Envelope xmlns:gesmes="http://www.gesmes.org/xml/2002-08-01" xmlns="http://www.ecb.int/vocabulary/2002-08-01/eurofxref">
+	<gesmes:subject>Reference rates</gesmes:subject>
+	<gesmes:Sender>
+		<gesmes:name>European Central Bank</gesmes:name>
+	</gesmes:Sender>
+	<Cube>
+		<Cube>
+		</Cube>
+	</Cube>
+</gesmes:Envelope>`
+
+var fooXml string = ""
